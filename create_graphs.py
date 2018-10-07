f9673438df5f2d2782e192d8281b97316a6b09e3
rlabausa <rubylabausa@gmail.com>
2018-10-07 01:18:22 -0500

    added index.html file and created graph functions

---
+++ create_graphs.py
@@ -0,0 +1,18 @@
+import pygal 
+
+def basic_bar(news_src, num_pos, num_neg, num_neut):
+    line_chart = pygal.Bar()
+    line_chart.title = 'Sentiment Across News Sources'
+    line_chart.x_labels = [news_src[0], news_src[1], news_src[2], news_src[3], news_src[4], news_src[5]]
+    line_chart.add('Positive',[num_pos[0], num_pos[1], num_pos[2], num_pos[3], num_pos[4], num_pos[5]])
+    line_chart.add('Negative',  [num_neg[0], num_neg[1], num_neg[2], num_neg[3], num_neg[4], num_neg[5]])
+    line_chart.add('Neutral',     [num_neut[0], num_neut[1], num_neut[2], num_neut[3], num_neut[4], num_neut[5]])
+    line_chart.render_to_file('basic_bar.svg')   
+      
+
+# src = ['NYT', 'WSJ', 'FOX', 'CNN', 'CBS', 'BBC']
+# pos = [1,2,3,4,5,6,7]
+# neg= [1,2,3,4,5,6,7]
+# neut= [1,2,3,4,5,6,7]
+# basic_bar(src, pos, neg, neut)
+
